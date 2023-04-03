ba257865c5de1f7f35d1142708ee9cce867936b1
Sasikumar Chandran <sasikumar.chandran@idinsight.com>
2023-04-03 11:39:17 +0530

    Initial commit with latest changes

---
+++ src/main/java/com/polus/core/FibiCoreApplication.java
@@ -0,0 +1,13 @@
+package com.polus.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FibiCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FibiCoreApplication.class, args);
+	}
+
+}
